ad0815366cfb48924a13a8a1498b4b2c4d037df1
SetjhabaK <setjhaba@gmail.com>
2017-06-14 09:41:31 +0200

    gps coordinates event producer using akka for server sent events.

--- atm-lambda/src/main/scala/layer/batch/batchJob.scala
+++ atm-lambda/src/main/scala/layer/batch/batchJob.scala
@@ -1,8 +1,6 @@
 package layer.batch
 
-/**
-  * Created by Phuti Rapheeha on 2017/06/06.
-  */
+
 object batchJob {
 
 }
--- atm-lambda/src/main/scala/layer/config/settings.scala
+++ atm-lambda/src/main/scala/layer/config/settings.scala
@@ -1,13 +1,11 @@
 package layer.config
 
 import com.typesafe.config.ConfigFactory
-/**
-  * Created by Phuti Rapheeha on 2017/06/06.
-  */
+
 object settings {
   private val config = ConfigFactory.load()
 
-  object particleReaderConf {
+  object ParticleReaderGen {
     private val particleReaderGen = config.getConfig("particle")
 
     lazy val filepath = particleReaderGen.getString("file_path")
