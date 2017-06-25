c2e73cfc4ba050321220cf99ef1d5528d3971791
Phuti Rapheeha <phuti@bbd.co.za>
2017-06-25 16:10:20 +0200

    BatchJob

--- atm-lambda/src/main/scala/layer/batch/BatchJob.scala
+++ atm-lambda/src/main/scala/layer/batch/BatchJob.scala
@@ -1,22 +1,94 @@
 package layer.batch
 
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+import kafka.serializer.StringDecoder
 import layer.config.Settings
-import utils.SparkUtils
+import layer.device.{EventJsonSerializer, EventProducer}
+import layer.domain.{Coordinate, CoordinateJsonSerializer}
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
 
 object BatchJob {
- def main(args:Array[String]) : Unit = {
+  import scala.language.implicitConversions
 
-   //setup spark context
-   val sc = SparkUtils.getSparkContext("Lambda with Spark")
-   val sqlContext = SparkUtils.getSQLContext(sc)
-   val plc = Settings.ParticleReaderGen
+  def main(args: Array[String]): Unit = {
 
-   //initialize input RDD
-   val inputDF = sqlContext.read.parquet(plc.hdfsPath)
-     .where("unix_timestamp() - timestamp_hour / 1000 <= 60 * 60 * 6")
+    val particleReaderGen = Settings.ParticleReaderGen
+    val cassandraHost = particleReaderGen.cassandraHost
+    val cassandraPort = particleReaderGen.cassandraPort
 
-   inputDF.registerTempTable("Location")
+    val conf =
+      new SparkConf()
+        .setAppName("AssetTracker")
+        .setMaster("local")// <--- This is what's missing
+        .set("spark.storage.memoryFraction", "1")
+        .set("spark.cassandra.connection.host", cassandraHost )
+        .set("spark.cassandra.connection.port", cassandraPort )
+        .set("spark.cassandra.connection.keep_alive_ms", "30000")
 
- }
+    val sc = new SparkContext(conf)
+
+    val streamingContext = new StreamingContext(sc, Seconds(4))
+    val topic = particleReaderGen.kafkaTopic
+
+    val kafkaDirectParams = Map(
+      "metadata.broker.list" -> particleReaderGen.kafkaBootstrapServers,
+      "group.id" -> "EventConsumer",
+      "auto.offset.reset" -> "largest"
+    )
+
+    val kafkaDirectStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+      streamingContext, kafkaDirectParams, Set(topic)
+    ).map(_._2)
+
+
+    val coordinateStream = kafkaDirectStream.transform(input => {
+
+      input.flatMap{ line =>
+
+        val isValid = EventJsonSerializer.tryParse(line)
+
+        if(isValid){
+
+          val event = EventJsonSerializer.deserializeEvent(line)
+          val coordinates = event.data.split(",")
+
+          if(event.coreid == particleReaderGen.deviceId &&
+            coordinates(0) != "offline" &&
+            coordinates(0) != "online") {
+
+            val coordinate = Coordinate(coordinates(0).toDouble,
+              coordinates(1).toDouble, event.published_at, event.coreid)
+
+            EventProducer.produceMessage(CoordinateJsonSerializer.serialize(coordinate), "test-topic")
+
+            Some(coordinate)
+          }
+          else{
+            None
+          }
+        }
+        else {
+          None
+        }
+      }
+    }).cache()
+
+
+    val dailyCoordinate = coordinateStream.map(coordinate => {
+      val today = Calendar.getInstance()
+      val dayFormat = new SimpleDateFormat("yyyy-MM-dd")
+
+    }
+
+    )
+
+
+    streamingContext.start()
+    streamingContext.awaitTermination()
+  }
 
 }
---
+++ atm-lambda/src/main/scala/layer/domain/DailyCoordinate.scala
@@ -0,0 +1,8 @@
+package layer.domain
+
+/**
+  * Created by Phuti Rapheeha on 2017/06/25.
+  */
+class DailyCoordinate {
+
+}
